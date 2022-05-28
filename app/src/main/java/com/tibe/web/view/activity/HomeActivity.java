e21d9779be5a7ce3ba09a0f79bafcdaa26d96eaa
Thiago Rodrigues <sr.ght@protonmail.ch>
2022-05-28 11:46:19 -0300

    add splash screen

--- app/src/main/java/com/tibe/web/view/activity/HomeActivity.java
+++ app/src/main/java/com/tibe/web/view/activity/HomeActivity.java
@@ -3,7 +3,10 @@ package com.tibe.web.view.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
+import android.inputmethodservice.KeyboardView;
 import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
 
 import com.tibe.web.R;
 import com.tibe.web.databinding.ActivityHomeBinding;
@@ -15,9 +18,28 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_Web);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         binding = DataBindingUtil.
         setContentView(this, R.layout.activity_home);
 
+        click();
+    }
 
+    private void click(){
+        binding.layoutHome.setOnClickListener(v ->{
+            InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(binding.layoutHome.getWindowToken(), 0);
+        });
+
+        binding.searchEngine.setOnClickListener(v ->{
+
+        });
     }
 }
